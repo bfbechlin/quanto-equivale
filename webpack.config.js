6ab88e78ec24258eee48d30dd1d717a690d36c0d
Beuren Bechlin <bfbechlin@inf.ufrgs.br>
2018-10-18 22:12:19 -0300

    Setting up enviorment

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const dashboardPlugin = new DashboardPlugin();
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: `${__dirname}/public/index.html`,
+  filename: './index.html',
+});
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  plugins: [
+    dashboardPlugin,
+    htmlWebpackPlugin,
+  ],
+};
