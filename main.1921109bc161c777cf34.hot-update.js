e7edfd20fbb2fea5615cd8ef2e3bf0ffacbea9a2
Beuren Bechlin <bfbechlin@inf.ufrgs.br>
2018-11-28 01:10:25 -0200

    Updates

---
+++ main.1921109bc161c777cf34.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/core/SearchOptions.js":
+/*!***********************************!*\
+  !*** ./src/core/SearchOptions.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar options = [{\n  id: '1',\n  label: 'Hospitais',\n  description: 'Gasto com hospitais testando as coisas tudo testeszan do adads mesas asdjia adsadsds.',\n  link: '',\n  img: '/assets/hospital.jpg'\n}, {\n  id: '2',\n  label: 'Escolas',\n  description: '\"Gasto com escolas testeszan do adads mesas asdjia adsadsds asd  asdiasdas asd asdadssda.\"',\n  link: '',\n  img: '/assets/hospital.jpg'\n}];\n/* harmony default export */ __webpack_exports__[\"default\"] = (options);\n\n//# sourceURL=webpack:///./src/core/SearchOptions.js?");
+
+/***/ })
+
+})
